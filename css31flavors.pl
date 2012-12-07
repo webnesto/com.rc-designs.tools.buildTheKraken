f9f40162d2dcb89f090cb10e3ff1147f565948cd
Christopher Goldman (au79) <dev@oolong.com>
2012-12-06 17:17:14 -0800

    I made the logic much simpler.  And added a "-n" flag to *NOT* actually do anything, just output what *would* be done.

--- css31flavors.pl
+++ css31flavors.pl
@@ -37,157 +37,207 @@ use File::Find;
 use File::Spec;
 use IO::File;
 use POSIX qw();
-use Utils qw( printLog );
 
 # Constants
 
 use constant {
-  MAX => 31
+  MAX   => 31
+, TRUE  => 1
+, FALSE => ''
+, HORIZ_LINE => '=' x 32
 };
 
-
-
+my $IMPORT_PATTERN = qr/^\@import/;
 
 # Stuff happens after here.
 
-printLog(
-  "++++++++++++++++"
-, "begin css31flavors: " . POSIX::strftime("%m/%d/%Y %H:%M:%S", localtime)
-);
-
-# Where are we now?
-my $originalDir = Cwd::getcwd();
+# Are we really going to do stuff?
+$main::forReals = TRUE;
 
 # Where are we going to do stuff?
-my $targetDir = Cwd::abs_path( shift || '.' );
+my @targetDirs = ();
 
-printLog( "target directory is $targetDir" );
-
-# Set up list storage for CSS filenames.
-my @cssFilePaths;
-
-# Define a subroutine reference for doing something with CSS files.
-my $cssFilePathSnarfer = getWantedSub( qr/\.css$/, \@cssFilePaths );
-
-# Go to the target directory.
-#chdir $targetDir;
-
-# Populate @cssFilePaths.
-find( $cssFilePathSnarfer, $targetDir );
-
-for my $path ( @cssFilePaths ) {
-
-  printLog( "full path : $path" );
-
-  my( $filename, $directories, $suffix ) = fileparse( $path, qr/\Q.css\E/ );
-  
-  printLog(
-    "basename  : $basename"
-  , "directory : $directories"
-  , "suffix    : $suffix"
-    );
-
-  my $fh = new IO::File $path, 'r';
-  unless( defined $fh ) {
-    carp "Unable to open '$path' for reading. Skipping.";
+# Process command-line arguments
+for( @ARGV ) {
+  if( $_ eq '-n' ) {
+    $main::forReals = FALSE;
     next;
   }
 
-  my @lines = <$fh>;
-  my @imports = grep { /^\@import/ } @lines;
-
-  printLog( "Import count: ". scalar @imports  );
-
-  if( scalar @imports > MAX ) {
-    printLog( ( scalar @imports ) . " > " . MAX . ': Creating subsets' );
+  unless( -d $_ ) {
+    carp "'$_' is not a directory.  Skipping.";
+    next;
   }
 
+  push @targetDirs, $_;
 }
 
-exit 0;
-
-
-foreach $css_file (@css_files) {
-  printLog( "file: $css_file" );
-  my($filename, $directories, $suffix) = fileparse($css_file, qr/\Q.css\E/);
-
-  printLog( "filename is: $filename" );
-
-  open FILE, "<$css_file";
-  my @lines = <FILE>;
-  @imports = ();
-
-  foreach $line (@lines){
-    if( $line =~ /^\@import/ ){
-      push( @imports, $line );
-    }
-  }
-
-  $imports = @imports;
-
-  printLog( "has this many imports: $imports" );
-
-  if( $imports > $MAX ){
-    printLog( "creating subsets" );
-    $import_count = 0;
-    $file_count = 0;
-
-    $tmpfile = File::Spec->catfile( $targetDir,  "$filename.tmp.css" );
-    open TMPFILE, ">$tmpfile";
-
-    &startSubfile(
-      $subfile
-    , $targetDir
-    , $filename
-    , $file_count
-    );
-
-    foreach $import  (@imports) {
-      print FILEPART $import;
-      $import_count = $import_count + 1;
-      if( $import_count == $MAX ){
-        $import_count = 0;
-        close FILEPART;
-        $file_count = $file_count + 1;
-        &startSubfile(
-          $subfile
-        , $targetDir
-        , $filename
-        , $file_count
-        );
-      }
-    }
-
-    close FILEPART;
-    close TMPFILE;
-  } else {
-    $tmpfile = 0;
-  }
-
-
-  close FILE;
-  if( $tmpfile ){
-    rename $tmpfile, $css_file;
-  }
-}
-
-printLog(
-  "end css31flavors:"
-.  POSIX::strftime("%m/%d/%Y %H:%M:%S", localtime)
-, "++++++++++++++++"
+logThis(
+  HORIZ_LINE
+, "Begin $0 : " . formattedTime()
 );
 
-sub startSubfile {
-  my (
-    $subfile
-  , $targetDir
-  , $filename
-  , $file_count
-  ) = @_;
+if( scalar @targetDirs == 0 ) {
+  push @targetDirs, Cwd::getcwd();
+}
 
-  $subfile = File::Spec->catfile( $targetDir,  "$filename"."_"."$file_count.css" );
-  printLog( "sub file: $subfile" );
-  open FILEPART, ">$subfile";
-  print TMPFILE "\@import \"$filename"."_"."$file_count.css\";\n";
+logThis( 'Target directories: ' . join( ':', @targetDirs ) );
+
+# Set up list storage for CSS filenames.
+my @cssFilePaths = ();
+
+# Define a subroutine reference for doing something with CSS files.
+my $cssFilePathSnarfer = getWantedSub( qr/\.css$/, \@cssFilePaths ); 
+
+# Populate @cssFilePaths.
+for my $targetDir ( @targetDirs ) {
+  find( $cssFilePathSnarfer, $targetDir );
+}
+@cssFilePaths = sort @cssFilePaths;
+
+
+# Iterate over every CSS file.
+for my $cssFilePath ( @cssFilePaths ) {
+
+  my( $cssFilename, $cssDir, $suffix ) = fileparse( $cssFilePath, qr/\Q.css\E/ );
+  
+  my $cssHandle = IO::File->new( $cssFilePath, 'r' );
+  unless( defined $cssHandle ) {
+    carp "Unable to open '$cssFilePath' for reading. Skipping.";
+    next;
+  }
+
+  my @lines = <$cssHandle>;
+
+  # Close the original CSS file.
+  undef $cssHandle;
+
+  # Classify lines
+  my @importLines = ();
+  my @nonImportLines = ();
+  for( @lines ) {
+    if( /$IMPORT_PATTERN/ ) {
+      push @importLines, $_;
+    }
+    else {
+      push @nonImportLines, $_;
+    }
+  }
+
+  logThis( "CSS file path: $cssFilePath [" . (scalar @importLines) . '/' . (scalar @nonImportLines) . ']'  );
+
+  # Do we need to rewrite the CSS file?
+  if( scalar @importLines > MAX ) {
+    
+    # If $cssDir is not writable, we cannot continue..
+    unless( -w $cssDir ) {
+      croak "Unable to replace files in '$cssDir': not writable.";
+    }
+
+    # How many subfiles will we need?
+    my $subFileCount = POSIX::ceil( ( scalar @importLines ) / MAX );
+
+    logThis( ( scalar @importLines ) . ' > ' . MAX . ": replacement file and $subFileCount subfiles needed" );
+
+    # Create the file that will replace the original.
+    my $tempFilePath = File::Spec->catfile( $cssDir, "$cssFilename.tmp.css" );
+    my $tempHandle = IO::File->new( ( $main::forReals ? $tempFilePath : File::Spec->devnull() ), 'w' );
+    unless( defined $tempHandle ) {
+      croak "Unable to open '$tempFilePath' for writing: $!";
+    }
+    logThis( "Created temp replacement file: '$tempFilePath'" );
+
+
+    for my $subFileIndex ( 0 .. ( $subFileCount - 1 ) ) {
+
+      # Zero-pad the index value.
+      if( $subFileIndex < 10 ) {
+        $subFileIndex = '0' . $subFileIndex; # Zero-padding
+      }
+
+      # Create the subfile that will hold up to MAX imports from the original parent.
+      my $subFileName = "${cssFilename}_sub_${subFileIndex}.css";
+      my $subFilePath = File::Spec->catfile( $cssDir, $subFileName );
+      my $subHandle = IO::File->new( ( $main::forReals ? $subFilePath : File::Spec->devnull() ), 'w' );
+      unless( defined $subHandle ) {
+        croak "Unable to open '$subFilePath' for writing: $!";
+      }
+      logThis( 'Created subfile ' . ( $subFileIndex + 1 ) . ": '$subFilePath'" );
+
+      # If MAX is 31, the ranges will be 0..31, 32..63, 64..95, etc.
+      for my $importLineIndex ( ( $subFileIndex * MAX ) .. ( ( ( $subFileIndex + 1 ) * MAX ) - 1 ) ) {
+        # The last subfile will likely not run all the way to the end.
+        if( $importLineIndex > $#importLines ) {
+          last;
+        }
+        my $importLine = $importLines[$importLineIndex];
+        
+        $subHandle->print( $importLine );
+        chomp $importLine;
+        logThis( "Adding subfile import: ${importLine}" );
+      }
+
+      # Close the subfile.
+      undef $subHandle;
+
+      # Write the subfile import into the replacement parent.
+      my $parentImportLine = "\@import url(\"${subFileName}\");";
+      $tempHandle->print( $parentImportLine . $/ );
+      logThis( "Adding replacement file import: ${parentImportLine}" );
+
+    }
+
+    # Write the non-import lines below the new subfile import lines.
+    for my $nonImportLine ( @nonImportLines ) {
+      $tempHandle->print( $nonImportLine );
+    }
+    logThis( 'Wrote remaining non-import lines to the replacement file.' );
+
+    # Close the temporary file.
+    undef $tempHandle;
+    
+    # Replace the original CSS file with the replacement.
+    if( $main::forReals ) {
+      unless( rename $tempFilePath, $cssFilePath ) {
+        croak "Unable to rename '$tempFilePath' to '$cssFilePath': $!";
+      }
+
+      logThis( "Renamed '$tempFilePath' to '$cssFilePath'." );
+    }
+
+  }
+  else {
+    logThis( ( scalar @importLines ) . ' <= ' . MAX . ": Skip this file." );
+  }
+
+  logThis( '' );
+
+}
+
+logThis(
+  "End $0 : " . formattedTime()
+, HORIZ_LINE
+);
+
+sub getWantedSub {
+  my $pattern = shift;
+  my $arrayRef = shift;
+
+  return sub {
+    if( /$pattern/ ) {
+      push @{$arrayRef}, $File::Find::name;
+    }
+  }
+}
+
+sub logThis {
+  local $, = $/; # Used to join elements in @_.
+  local $\ = $/; # Used at the end of the print.
+
+  print { $main::forReals ? *STDOUT : *STDERR } @_;
+}
+
+sub formattedTime {
+  return POSIX::strftime( '%m/%d/%Y %H:%M:%S', localtime );
 }
 
